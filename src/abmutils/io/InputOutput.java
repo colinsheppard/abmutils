eef0132cf7a718384ec102b7d577e2df37ed194d
Colin Sheppard <colinthegaunt@gmail.com>
2015-04-30 10:10:20 -0700

    alternative constructor

--- src/abmutils/io/InputOutput.java
+++ src/abmutils/io/InputOutput.java
@@ -12,6 +12,9 @@ public class InputOutput {
 
 	public InputOutput() {
 	}
+	public static Table readTable(File dataFile) throws IOException{
+		return new Table(new FileInputStream(dataFile),FilenameUtils.getExtension(dataFile.getName()).toLowerCase() + ",header");
+	}
 	public static Table readTable(File columnFile, File dataFile) throws IOException{
 		Table columnTable = new Table(new FileInputStream(columnFile),FilenameUtils.getExtension(columnFile.getName()).toLowerCase() + ",header");
 		Table dataTable = columnTable.typedParse(new FileInputStream(dataFile), FilenameUtils.getExtension(dataFile.getName()).toLowerCase() + ",header");
